3f7377e24719580db84d951c1e9a2de36243d82c
mbrev <mbrev@LAPTOP-2DN58FBI>
2022-01-12 22:07:06 -0700

    Add project files.

---
+++ FirstProject.vb
@@ -0,0 +1,17 @@
+﻿'Miranda Breves
+'RCET 265
+'Spring 2022
+'My First VB.Net Program
+'github url
+
+Module FirstProject
+
+    Sub Main()
+
+        Console.WriteLine("Hello World!")
+        Console.Beep()
+        Console.ReadLine()
+
+    End Sub
+
+End Module
